a46f1397e511aa88eca3c609eb8e0f317d2004f0
smriti2805 <smriti82929@gmail.com>
2025-04-04 09:05:50 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:seed/providers/auth_provider.dart';
@@ -5,8 +7,19 @@ import 'package:seed/theme/theme_provider.dart';
 import 'package:seed/home_page.dart';
 import 'package:seed/theme/theme.dart';
 
-void main() {
-  // firebase.ini
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(options: FirebaseOptions(  apiKey: "AIzaSyAbkYNFnK9PN9cEE4ZBJxz8MZRYxU-dAAc",
+        authDomain: "seed-git.firebaseapp.com",
+        projectId: "seed-git",
+        storageBucket: "seed-git.firebasestorage.app",
+        messagingSenderId: "1092252675503",
+        appId: "1:1092252675503:web:d84da76e69e67e8150535a"));
+  }else{
+    await Firebase.initializeApp();
+  }
+
   runApp(
     MultiProvider(
       providers: [
